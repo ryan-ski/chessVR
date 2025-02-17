fead9164e1213588aaa0614db00ec9da611acb88
ryan-ski <124533292+ryan-ski@users.noreply.github.com>
2025-02-16 19:20:23 -0500

    knight.cs
    add  to knight prefab

---
+++ knight.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : ChessPiece
+{
+    // Possible moves for the knight
+    private static readonly Vector2Int[] moveOffsets = 
+    {
+        new Vector2Int(2, 1),  new Vector2Int(2, -1),
+        new Vector2Int(-2, 1), new Vector2Int(-2, -1),
+        new Vector2Int(1, 2),  new Vector2Int(1, -2),
+        new Vector2Int(-1, 2), new Vector2Int(-1, -2)
+    };
+
+    public override List<Vector2Int> GetValidMoves(ChessPiece[,] boardState)
+    {
+        List<Vector2Int> validMoves = new List<Vector2Int>();
+
+        // Check each possible move for the knight
+        foreach (Vector2Int offset in moveOffsets)
+        {
+            Vector2Int newPosition = new Vector2Int(boardPosition.x + offset.x, boardPosition.y + offset.y);
+            if (IsWithinBoard(newPosition))
+            {
+                ChessPiece targetPiece = boardState[newPosition.x, newPosition.y];
+                if (targetPiece == null || targetPiece.isWhite != this.isWhite)
+                    validMoves.Add(newPosition); // Valid move if empty or enemy piece not blocking
+            }
+        }
+
+        return validMoves;
+    }
+
+    private bool IsWithinBoard(Vector2Int position)
+    {
+        return position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8;
+    }
+}
